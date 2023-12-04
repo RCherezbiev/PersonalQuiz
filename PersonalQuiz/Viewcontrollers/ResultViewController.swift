267513940a964e23d0d44096085b3a16bcbbc666
RCherezbiev <149434791+RCherezbiev@users.noreply.github.com>
2023-12-05 02:52:59 +0300

    Final lesson code

---
+++ PersonalQuiz/Viewcontrollers/ResultViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Rustam Cherezbiev on 04.12.2023.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func doneButtonAction(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+}
